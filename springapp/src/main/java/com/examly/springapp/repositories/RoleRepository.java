c99ff02a6e29c4c30ce5d62e6c4678cae06dd563
JayaKumar01 <jayakumar.ramu1201@gmail.com>
2022-03-30 17:07:35 +0000

    Fixed bug in spring

--- springapp/src/main/java/com/examly/springapp/repositories/RoleRepository.java
+++ springapp/src/main/java/com/examly/springapp/repositories/RoleRepository.java
@@ -1,9 +1,8 @@
 package com.examly.springapp.repositories;
 
 import com.examly.springapp.models.Role;
-
+import com.examly.springapp.models.ERole;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +10,5 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
   
-
 	Optional<Role> findByName(ERole name);
 }
