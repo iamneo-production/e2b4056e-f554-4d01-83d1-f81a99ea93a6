579c6e049af4722f0fe34d2524a6da9a9062c2e2
Rupankar99 <57449029+Rupankar99@users.noreply.github.com>
2022-04-18 14:05:21 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,20 +1,58 @@
-import Login from './Pages/Login'
-import UserDashboard from './Pages/UserDashboard'
+import { Route, Routes } from 'react-router-dom';
+import Login from './component/Pages/Login';
+import Registration from './component/Pages/Registration';
+import UserDashboard from './component/User/UserDashboard';
+import PrivateRoute from './component/Pages/PrivateRoute';
+import AdminDashboard from './component/Admin/AdminDashboard';
+import AddAcademy from './component/Admin/AddAcademy';
+import AccessDenied from './component/Pages/AccessDenied';
+import EnrollCourse from './component/User/EnrollCourse';
+import UserViewCourse from './component/User/UserViewCourse';
+import ViewCourses from './component/Admin/ViewCourses';
+import AddCourse from './component/Admin/AddCourse';
+import EnrollingCourse from './component/User/EnrollingCourse';
+import EnrolledCourse from './component/User/EnrolledCourse';
+import StudentList from './component/Admin/StudentList';
+import StudentDetails from './component/Admin/StudentDetails';
 
-export default function App() {
- 
+function App() {
   return (
-    <Router>
+    <div className="App" >
       <div>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/signup" element={<Registration />}></Route>
 
-        <div className="App">
-          <Routes>
-            <Route element={<Login/>} path="/"></Route>
-            <Route  element={<UserDashboard/>}  path="/dashboard"></Route>
-          </Routes>
-        </div>
+          {/*For Private Routes*/}
+          <Route element={<PrivateRoute />}>
+
+            {/*Routes for user role*/}
+            <Route element={<PrivateRoute rolerequired="ROLE_USER" />}>
+              <Route path="/user-dashboard" element={<UserDashboard />}></Route>
+              <Route path='/enrollcourse' element={<EnrollCourse />}></Route>
+              <Route path='/:instituteId/courses' element={<UserViewCourse />}></Route>
+              <Route path='/course-enroll/:courseId' element={<EnrollingCourse />}></Route>
+              <Route path='/enrolledcourses' element={<EnrolledCourse />}></Route>
+            </Route>
+
+            {/*Routes for admin role*/}
+            <Route element={<PrivateRoute rolerequired="ROLE_ADMIN" />}>
+              <Route path="/add-academy" element={<AddAcademy />}></Route>
+              <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+              <Route path="/edit-academy/:instituteId" element={<AddAcademy />}></Route>
+              <Route path='/academy/:instituteId/courses' element={<ViewCourses />}></Route>
+              <Route path="/academy/:instituteId/create-course" element={<AddCourse />}></Route>
+              <Route path="/edit-course/:courseId" element={<AddCourse />}></Route>
+              <Route path='/view-students' element={<StudentList />}></Route>
+              <Route path='/student-details/:admissionid' element={<StudentDetails />}></Route>
+            </Route>
+          </Route>
+
+          <Route path="/accessdenied" element={<AccessDenied />} />
+        </Routes>
       </div>
-      </Router>
+    </div>
   );
 }
 
+export default App;
