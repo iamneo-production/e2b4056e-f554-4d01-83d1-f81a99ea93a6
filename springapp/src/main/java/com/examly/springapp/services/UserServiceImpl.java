5cfbac63b08de9ef702d754d0f66e5b8b742cd51
Rupankar99 <57449029+Rupankar99@users.noreply.github.com>
2022-04-22 22:05:04 +0530

    Update UserServiceImpl.java

--- springapp/src/main/java/com/examly/springapp/services/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/services/UserServiceImpl.java
@@ -1,21 +1,20 @@
-package com.examly.springapp.services;
+package com.example.demo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import com.examly.springapp.models.CourseModel;
-import com.examly.springapp.models.StudentModel;
-import com.examly.springapp.models.User;
-import com.examly.springapp.payload.response.MessageResponse;
-import com.examly.springapp.payload.request.StudentRequest;
-import com.examly.springapp.repositories.CourseRepository;
-import com.examly.springapp.repositories.StudentRepository;
-import com.examly.springapp.repositories.UserRepository;
+import com.example.demo.models.CourseModel;
+import com.example.demo.models.StudentModel;
+import com.example.demo.models.User;
+import com.example.demo.payload.Response.MessageResponse;
+import com.example.demo.payload.request.StudentRequest;
+import com.example.demo.repository.CourseRepository;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.repository.UserRepository;
 
 @Component
 public class UserServiceImpl implements UserService{
@@ -27,37 +26,46 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	UserRepository userRepository;
 	
+	
 	@Override
 	public ResponseEntity<?> enroll(StudentRequest studentRequest) {
 			
-		
+			//System.out.println(studentRequest.getUserid());
 			
 			List<Long> user_id = studentRepository.getUserId(studentRequest.getUserid());
 			
 				if(user_id.get(0) > 0)
 				{
-					
+					//System.out.println("if");
 					
 				List<Long> studentid = studentRepository.getStudentId(studentRequest.getUserid());
 				Long student_id=studentid.get(0);
-				StudentModel student = new StudentModel( student_id,studentRequest.getFirstName(), studentRequest.getFatherName(),
-						studentRequest.getMotherName(), studentRequest.getEmail(), 
-						studentRequest.getGender(), studentRequest.getState(), 
-	studentRequest.getNationality(), studentRequest.getPincode(),studentRequest.getPhonenumber(), studentRequest.getAddress());
+				StudentModel student = new StudentModel( student_id,
+						studentRequest.getFirstName(), 
+						studentRequest.getFatherName(),
+						studentRequest.getMotherName(), 
+						studentRequest.getAge(),
+						studentRequest.getEmail(), 
+						studentRequest.getGender(), 
+						studentRequest.getState(), 
+						studentRequest.getNationality(), 
+						studentRequest.getPhonenumber(), 
+						studentRequest.getAddress(),
+						studentRequest.getPincode());
 				
 	
-				Optional <CourseModel> courses=courseRepository.findById(studentRequest.getCourse_id());
-				student.getCourses().add(courses.get());
-				Optional<User> u=userRepository.findById( studentRequest.getUserid());
+				CourseModel courses=courseRepository.getById(studentRequest.getCourse_id());
+				student.getCourses().add(courses);
+				User u=userRepository.getById((long) studentRequest.getUserid());
 				
-				student.setUsers(u.get());
-			
+				student.setUsers(u);
+				System.out.println(student.toString());
 				studentRepository.save(student);
 			
 				return new ResponseEntity<>( student,HttpStatus.OK);
 				}
 				else if(user_id.get(0) == 0) {
-				
+				//	System.out.println("else if");
 				      Long random_int = (long)Math.floor(Math.random()*(2999-1000+1)+1000);
 	
 				       StudentModel student=new StudentModel( 
@@ -65,19 +73,19 @@ public class UserServiceImpl implements UserService{
 				    		   	studentRequest.getFirstName(), 
 				    		   	studentRequest.getFatherName(),
 								studentRequest.getMotherName(), 
+								studentRequest.getAge(),
 								studentRequest.getEmail(), 
 								studentRequest.getGender(), 
 								studentRequest.getState(), 
 								studentRequest.getNationality(), 
-								studentRequest.getPincode(),
 								studentRequest.getPhonenumber(), 
-								studentRequest.getAddress()
-								);
-					Optional<CourseModel> courses=courseRepository.findById(studentRequest.getCourse_id());
-					student.getCourses().add(courses.get());
-			      
-				     Optional< User> u=userRepository.findById( studentRequest.getUserid());
-				student.setUsers(u.get());
+								studentRequest.getAddress(),
+								studentRequest.getPincode());
+						CourseModel courses=courseRepository.getById(studentRequest.getCourse_id());
+					student.getCourses().add(courses);
+				      
+				      User u=userRepository.getById((long) studentRequest.getUserid());
+				student.setUsers(u);
 						
 						studentRepository.save(student);
 						 return new ResponseEntity<>( student,HttpStatus.OK);
