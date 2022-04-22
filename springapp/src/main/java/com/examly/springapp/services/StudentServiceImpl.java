bf67c10283d5ba7177c8a2b72be6297d208fc424
Rupankar99 <57449029+Rupankar99@users.noreply.github.com>
2022-04-22 22:03:53 +0530

    Update StudentServiceImpl.java

--- springapp/src/main/java/com/examly/springapp/services/StudentServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/services/StudentServiceImpl.java
@@ -1,4 +1,5 @@
-package com.examly.springapp.services;
+package com.example.demo.services;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,8 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.examly.springapp.models.StudentModel;
-import com.examly.springapp.repositories.StudentRepository;
+import com.example.demo.exceptions.ResourceNotFoundException;
+import com.example.demo.models.StudentModel;
+import com.example.demo.repository.StudentRepository;
 
 @Service
 public class StudentServiceImpl implements StudentService{
@@ -19,10 +21,36 @@ public class StudentServiceImpl implements StudentService{
 		return new ResponseEntity<>(list , HttpStatus.OK);
 	}
 	
-	public ResponseEntity<StudentModel> viewStudentById(Long Studentid){
+	public ResponseEntity<StudentModel> viewAdmissionById(Long Studentid){
 		StudentModel student = studentrepo.findById(Studentid)
 		.orElseThrow(()-> new ResourceNotFoundException("No student found with ID "+Studentid));
 		return new ResponseEntity<>(student,HttpStatus.OK);
+	}
+	
+	public ResponseEntity<List<StudentModel>> viewStudentById(Long Studentid){
+		List<StudentModel> list  = studentrepo.findStudentByStudentid(Studentid);
+		return new ResponseEntity<>(list,HttpStatus.OK);
+	}
+	
+	public ResponseEntity<List<StudentModel>> editStudentById(Long Studentid , StudentModel student){
+		
+		List<StudentModel> list = studentrepo.findStudentByStudentid(Studentid);
+		for(StudentModel existingStudent : list) {
+			existingStudent.setAddress(student.getAddress());
+			existingStudent.setAge(student.getAge());
+			//existingStudent.setCourses(student.getCourses());
+			existingStudent.setEmail(student.getEmail());
+			existingStudent.setFathersname(student.getFathersname());
+			existingStudent.setGender(student.getGender());
+			existingStudent.setMothersname(student.getMothersname());
+			existingStudent.setNationality(student.getNationality());
+			existingStudent.setPhonenumber(student.getPhonenumber());
+			existingStudent.setPincode(student.getPincode());
+			existingStudent.setState(student.getState());
+			existingStudent.setStudentname(student.getStudentname());
+			studentrepo.save(existingStudent);
+		}
+		return new ResponseEntity<>(list,HttpStatus.OK);
 	}
 	
 	public ResponseEntity<HttpStatus> deleteStudentModel(Long Studentid){
