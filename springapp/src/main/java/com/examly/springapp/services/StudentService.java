d0fad892df0d85a2e527101ac4c510ea991c29ac
Rupankar99 <57449029+Rupankar99@users.noreply.github.com>
2022-04-22 22:03:04 +0530

    Update StudentService.java

--- springapp/src/main/java/com/examly/springapp/services/StudentService.java
+++ springapp/src/main/java/com/examly/springapp/services/StudentService.java
@@ -1,19 +1,26 @@
-package com.examly.springapp.services;
+package com.example.demo.services;
 
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.examly.springapp.models.StudentModel;
+import com.example.demo.models.StudentModel;
 
 public interface StudentService {
 	
 	//View all students
 	public ResponseEntity<List<StudentModel>> viewAllStudents();
 	
+	//View admission by id
+	public ResponseEntity<StudentModel> viewAdmissionById(Long Studentid);
+	
 	//View student by studentId
-	public ResponseEntity<StudentModel> viewStudentById(Long Studentid);
+	public ResponseEntity<List<StudentModel>> viewStudentById(Long Studentid);
+	
+	//Edit student
+	public ResponseEntity<List<StudentModel>> editStudentById(Long Studentid , StudentModel student);
+	
 	
 	//Delete student
 	public ResponseEntity<HttpStatus> deleteStudentModel(Long Studentid);
